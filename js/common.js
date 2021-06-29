3b3a3a35ea0ff3373f60e63a9d2cda5a9b28c372
html2021-2 <html2021-2@daum.net>
2021-06-29 10:40:41 +0900

    dep3 네비게이션 스크립트
    2021.0629.10:40

--- js/common.js
+++ js/common.js
@@ -1,4 +1,77 @@
 $(document).ready(function () {
+  //pc, mobile 헤더 공통
+  //스크롤
+  $(window).on('scroll', function () {
+    if ($(this).scrollTop() > 10) $('.header').addClass('on');
+    else $('#pcHeader').removeClass('on');
+  });
+
+  //언어선택 - 열기
+  $('#pcHeader .lang button').on('mouseenter click focusin', function () {
+    $(this).next().stop().slideDown('fast');
+  });
+  //닫기
+  $('#pcHeader .lang').on('mouseleave focusout', function () {
+    $(this).children('ul').stop().slideUp('fast');
+  });
+
+  //pcGnb
+  //뎁스1 li에 마우스, 키보드 진입
+  const $pcGnb = $('#pcGnb > ul');
+  const $gnbDep2 = $pcGnb.find('.dep2');
+  const $gnbDep3 = $gnbDep2.find('.dep3');
+  //depth2,배너슬라이더 숨기기
+  $gnbDep2.hide();
+  $gnbDep3.hide();
+  $('#pcGnb .swiper-container').hide();
+  //뎁스1 a: 마우스, 키보드 진입하면
+  $pcGnb.find('> li > a').on('mouseenter focus', function () {
+    //초기화: 먼저 열려진 뎁스1 li.on 제거, 자식 ul 숨기기
+    $pcGnb.find('> li.on').removeClass('on').children('.dep2').hide().parents('ul').next().hide();
+    //현재 활성화
+    $(this).next().show().parent().addClass('on').parents('ul').next().show();
+    //헤더.active추가
+    $('#pcHeader').addClass('active');
+  });
+
+  // 뎁스2 a a: 마우스, 키보드 진입하면
+  $gnbDep2.find('>ul>li>a').on('mouseenter focus', function () {
+    // 현재 선택된 a의 바로뒤 ul은 보이게 하고 부모 li.on 추가시키고,  나머지 형제는 뎁스2 li.on 제거 열려진 하위 ul 숨기기
+    if ($(this).next().length !== 0) { //뎁스3가 있는 경우
+      $(this).next().show().parent().addClass('on').siblings('li.on').removeClass('on').children('ul').hide();
+    } 
+    // 현재 선택된 a의 부모 li.on만 추가시키고,  나머지 형제는 뎁스2 li.on 제거 열려진 하위 ul 숨기기
+    else { //뎁스3가 없는 경우
+      $(this).parent().addClass('on').siblings('li.on').removeClass('on').children('ul').hide();
+    }
+  });
+
+  //마우스 이탈 - 뎁스1 ul에서 마우스 나오면 처음으로 되돌리기-초기화
+  $pcGnb.on('mouseleave', function () {
+    //초기화-> 먼저 열려진 뎁스1 li.on 제거, 자식 ul 숨기기
+    $(this).find('> li.on').removeClass('on').children('.dep2').hide().parents('ul').next().hide();
+    $('#pcHeader').removeClass('active');
+  });
+  //gnb 첫번째 a와 마지막 a에서 포커스가 나가면(blur) 뎁스1 ul에 mouseleave를 강제 호출
+  $pcGnb.find('a:first, a:last').on('blur', function () {
+    setTimeout(function () {
+      if( !$('#pcGnb a').if(':focus')) $pcGnb.mouseleave();
+    }, 10);
+  });
+
+  $pcGnb.find('a').first().on('keydown', function (e) {
+    if ((e.shiftKey === true && e.keyCode === 9) || (e.keyCode === 16)) $pcGnb.trigger('mouseleave');
+  });
+  $pcGnb.find('a').last().on('keydown', function (e) {
+    if ((!e.shiftKey === true && e.keyCode === 9) || (!e.keyCode === 16)) $pcGnb.trigger('mouseleave');
+  });
+
+
+
+ 
+
+
+
   //header : 배너 슬라이더
   const swiper = new Swiper('.swiper-container', {
     // Optional parameters
@@ -15,7 +88,7 @@ $(document).ready(function () {
   $('#pcGnb .swiper-container').on('mousemove', function (e) {
     const mouseX = e.pageX;
     const mouseY = e.pageY;
-    console.log(mouseX, mouseY);
+    //console.log(mouseX, mouseY);
     gsap.to('#cursorDrag', {left: mouseX, top: mouseY}); 
   });
 
--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
-  //cnt1 - pc, mobile 공통
-  //slick 슬라이더
   //#pcCnt1
-  //slick 슬라이더
+  //slick 슬라이더 - pc
   $('#pcCnt1 .slick-wrapper').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -10,52 +8,87 @@ $(document).ready(function () {
     vertical: false,
 
     dots: true,
-    dotsClass: 'slick-dots',
-    appendDots: $('.slick-pagination'),
+    dotsClass: 'pc-slick-dots',
+    appendDots: $('.pc-slick-pagination'),
     infinite: true,
     speed: 500,
     fade: true,
     cssEase: 'linear',
   });
 
-  // PC 버젼일 경우만 왼쪽 이미지 클릭시 다음으로 전환
-  $('#pcCnt1 .slide_bg img').on('click', function() {
-    if ($(window).width() >= 1440) {
-      var slideNo = $(this).parents('.slick-slide').data('slick-index');
-      console.log(slideNo);
-      $('#pcCnt1 .slick-wrapper').slick('slickGoTo', slideNo+1);
+  //마우스따라다니기
+  $('#pcCnt1 .slick-slide .slide_bg').on({
+    mousemove: function (e) {
+      const mouseX = e.pageX;
+      const mouseY = e.pageY;
+      console.log(mouseX, mouseY);
+      $(this).parents('.slick-container').next().css('visibility', 'visible');
+      gsap.to('.cursorDrag', {left: mouseX + 20,top: mouseY,duration: 0.2})
+    },
+    mouseleave: function () {
+      $(this).parents('.slick-container').next().css('visibility', 'hidden');
     }
   });
 
-  //마우스따라다니기
-  $('#pcCnt1').on('mousemove', function (e) {
-    const mouseX = e.pageX;
-    const mouseY = e.pageY;
-    // console.log(mouseX, mouseY);
-    $(this).find('.drag').css('visibility', 'visible');
-    gsap.to('.drag', {left: mouseX + 20, top: mouseY, duration: 0.2})
-
-  });
-  $('#pcCnt1').on('mouseleave', function () {
-    $(this).find('.drag').css('visibility', 'hidden');
+  //pc슬라이더 클릭 이벤트 추가 - 왼쪽 이미지 클릭하면 다음슬라이더로 이동하기
+  $('#pcCnt1 .slide_bg img').on('click', function () {
+    if ($(window).width() >= 1440) {
+      let slideNo = $(this).parents('.slick-slide').data('slick-index');
+      console.log(slideNo);
+      $('#pcCnt1 .slick-wrapper').slick('slickGoTo', slideNo + 1);
+    }
   });
 
   //#mCnt1
-  //slick 슬라이더
+  //slick 슬라이더 - mobile
   $('#mCnt1 .slick-wrapper').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     vertical: false,
 
-    //autoplay: true,  //자동실행
-    speed: 2000, //이전, 다음 버튼을 클릭하면 전환되는 시간 - 기본값 300
+    dots: true,
+    dotsClass: 'm-slick-dots',
+    appendDots: $('.m-slick-pagination'),
+    autoplay: true, 
+    speed: 2000, //슬라이드 전환되는 시간 - 기본값 300
     autoplaySpeed: 4000,
     easing: 'easeInOutQuint',
     pauseOnHover: false, //마우스 오버하면 슬라이더 정지
-    prevArrow: $('.slick-prev'), //이전 버튼 선택자
-    nextArrow: $('.slick-next'), //다음 버튼 선택자
     draggable: false, //드래그 가능 여부
+  });
+
+  //play, stop버튼
+  $('#mCnt1 .slick-auto button').on('click', function () {
+    // 첫번째 버튼이 일시정지인 경우
+    if ($(this).index() === 0) $('#mCnt1 .slick-wrapper').slick('slickPause');
+    // 그렇지 않고 두번째 버튼이 자동실행인 경우
+    else $('#mCnt1 .slick-wrapper').slick('slickPlay');
+    // 공통으로 클릭한 버튼은 숨기고 나머지 형제를 보이게 처리
+    $(this).removeClass('visible').siblings().addClass('visible');
+  });
+  $('#mCnt1 .slick-pause').addClass('visible');
+
+  //polygon 좌표 변경
+  $(window).on('resize', function () {
+    const winWid = $(this).width();
+    const winHei = $(this).height();
+    let point2, point3;
+    if(winWid <= 414) {
+      point2 = winWid * 0.4;
+      point3 = winWid * 0.98;
+      $('.slick-slide svg polygon').attr({points: `0 0, 0 ${winHei}, ${point3} ${winHei}, ${point2} 0`});
+      $('.slick-slide .reverse polygon').attr({points: `0 0, 0 ${winHei}, ${point2} ${winHei}, ${point3} 0`});
+    } else if(winWid <= 768) {
+      point2 = winWid * 0.4;
+      point3 = winWid * 0.8;
+      $('.slick-slide svg polygon').attr({points: `0 0, 0 ${winHei}, ${point3} ${winHei}, ${point2} 0`});
+      $('.slick-slide .reverse polygon').attr({points: `0 0, 0 ${winHei}, ${point2} ${winHei}, ${point3} 0`});
+    }
 
   });
+  $(window).trigger('resize');
+
+
 });
+
