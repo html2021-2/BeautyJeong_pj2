289e136667a691d39a64960068529b842f4eff97
html2021-2 <html2021-2@daum.net>
2021-06-30 16:08:45 +0900

    본문4 라이브까지 남은 시간 스크립트
    2021.0630.16:08

--- js/common.js
+++ js/common.js
@@ -30,33 +30,26 @@ $(document).ready(function () {
     $pcGnb.find('> li.on').removeClass('on').children('.dep2').hide().parents('ul').next().hide();
     //현재 활성화
     $(this).next().show().parent().addClass('on').parents('ul').next().show();
-    //헤더.active추가
-    $('#pcHeader').addClass('active');
+    //헤더.active추가 : 뎁스2 없으면 추가하지 말기
+    if ($(this).next().length !== 0) $('#pcHeader').addClass('active');
+    else $('#pcHeader').removeClass('active');
   });
 
-  // 뎁스2 a a: 마우스, 키보드 진입하면
-  $gnbDep2.find('>ul>li>a').on('mouseenter focus', function () {
-    // 현재 선택된 a의 바로뒤 ul은 보이게 하고 부모 li.on 추가시키고,  나머지 형제는 뎁스2 li.on 제거 열려진 하위 ul 숨기기
-    if ($(this).next().length !== 0) { //뎁스3가 있는 경우
+  //뎁스2 a : 마우스, 키보드 진입하면
+  $gnbDep2.find('> ul > li > a').on('mouseenter focus', function () {
+    //현재 선택된 a의 바로 뒤 ul(뎁스3)은 보이게 하고 부모 li.on추가, 나머지 형제는 .on제거 및 열려진 뎁스3 숨기기
+    if ($(this).next().length !== 0) {//뎁스2 있는 경우
       $(this).next().show().parent().addClass('on').siblings('li.on').removeClass('on').children('ul').hide();
-    } 
-    // 현재 선택된 a의 부모 li.on만 추가시키고,  나머지 형제는 뎁스2 li.on 제거 열려진 하위 ul 숨기기
-    else { //뎁스3가 없는 경우
-      $(this).parent().addClass('on').siblings('li.on').removeClass('on').children('ul').hide();
     }
+    //뎁스3가 없는 경우 현재 선택된 a의 부모 li.on만 추가시키고, 나머지 형제는 뎁스2 .on제거 및 뎁스3 숨기기
+    else $(this).parent().addClass('on').siblings('li.on').removeClass('on').children('ul').hide();
   });
 
   //마우스 이탈 - 뎁스1 ul에서 마우스 나오면 처음으로 되돌리기-초기화
   $pcGnb.on('mouseleave', function () {
-    //초기화-> 먼저 열려진 뎁스1 li.on 제거, 자식 ul 숨기기
+    //초기화 : 먼저 열려진 뎁스1 li.on 제거, 자식 ul과 배너슬라이더 숨기기
     $(this).find('> li.on').removeClass('on').children('.dep2').hide().parents('ul').next().hide();
     $('#pcHeader').removeClass('active');
-  });
-  //gnb 첫번째 a와 마지막 a에서 포커스가 나가면(blur) 뎁스1 ul에 mouseleave를 강제 호출
-  $pcGnb.find('a:first, a:last').on('blur', function () {
-    setTimeout(function () {
-      if( !$('#pcGnb a').if(':focus')) $pcGnb.mouseleave();
-    }, 10);
   });
 
   $pcGnb.find('a').first().on('keydown', function (e) {
@@ -66,9 +59,18 @@ $(document).ready(function () {
     if ((!e.shiftKey === true && e.keyCode === 9) || (!e.keyCode === 16)) $pcGnb.trigger('mouseleave');
   });
 
+  //배너슬라이더에 마우스,포커스 진입시 gnb.on
+  $('#pcGnb .swiper-container').on('mouseenter focusin', function () {
+    //현재 활성화
+    $pcGnb.find('> li > a').next().show().parents('ul').next().show();
+    $('#pcHeader').addClass('active');
+  });
 
-
- 
+  //배너슬라이더에 마우스,포커스 이탈시: 마우스가 아직 헤더 배경안에 있다면 뎁스2 ul은 닫아주지 말고 마우스가 헤더를 벗어났다면 배너 슬라이더와 해더배경과 뎁스2 ul도 닫아주기
+  $('#pcGnb .swiper-container').on('mouseleave focusout', function () {
+    $pcGnb.find('> li > a').next().hide().parents('ul').next().hide();
+    $('#pcHeader').removeClass('active');
+  });
 
 
 
